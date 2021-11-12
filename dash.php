72f2c60c485dc4815c86cc5329550b720a547d71
turvec <okaforturvec@gmail.com>
2021-11-12 03:55:43 -0800

    commit

--- dash.php
+++ dash.php
@@ -2,54 +2,17 @@
  include_once "header.php";
 ?>
 
-<!-- header Start -->
-<div class="row bg-custom">
-    <div class="col-12">
-        <div class="my-4 mx-2 p-4 d-flex justify-content-between">
-            <div class="brand">
-                <i class=" fab fa-gg-circle fa-2x"></i>
-                <span>VICTOR(VV) WALLET</span>
-            </div>
-            <h5 class="text-white">Turvec Okafor</h5>
+<div class="row">
 
-        </div>
-
-    </div>
+<div class="col-md-3">
+    <div class="card card-body"></div>
 </div>
-<!-- header Ends -->
-<div class="page-content ">
-    <div class="row mx-5">
-        <div class="col-md-3">
-            <div class="sidebar card card-body shadow">
-                <div class="icon1 text-right ">
-                    <i class="fas fa-ellipsis-v b1"></i>
-                </div>
-                <div class="avatar text-center">
-                    <div class=" d-flex justify-content-center">
-                        <div class="avatar-circle">
-                            <img src="assets/img/dog.jpg" alt="avatar">
-                        </div>
-                    </div>
-                    <h5 class="name">Turvec Okafor</h5>
-                    <p class="name">Admin</p>
-                </div>
-                <div class="icon2 ">
-                    <i class="fas fa-columns p-2 b1"> : Dashboard</i><br>
-                    <i class="fas fa-wallet p-2 b1"> : My wallet</i><br>
-                    <i class="fas fa-user-shield p-2 b1"> : security</i><br>
-                    <i class="fas fa-fingerprint p-2 b1"> : Authentification</i><br>
-                    <i class="fas fa-award p-2 b1"> : Rewards</i><br>
-                    <i class="fas fa-tasks p-2 b1"> : More options</i>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-9">
-            <div class="page-content1 card card-body" style="height:500px;">
-            </div>
-        </div>
-    </div>
+<div class="col-md-9">
+    <div class="card card-body"></div>
+</div>
 
 </div>
+
 <?php
 include_once "footer.php";
 ?>
